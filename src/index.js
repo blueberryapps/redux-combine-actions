041711de3acde5b552552d3ad948afba035edd32
Ondrej Bartas <ondrej@bartas.cz>
2015-11-09 22:07:48 +0100

    Fix configuration action

--- src/index.js
+++ src/index.js
@@ -7,39 +7,41 @@ const defaultTypes = ['PENDING', 'FULFILLED', 'REJECTED'];
 export default function sequenceMiddleware(config = {}) {
     const promiseTypeSuffixes = config.promiseTypeSuffixes || defaultTypes;
 
-    return next => action => {
-        if (!isArrayOfFunctions(action.payload)) {
-            return next(action);
-        }
+    return () => {
+        return next => action => {
+            if (!isArrayOfFunctions(action.payload)) {
+                return next(action);
+            }
 
-        const { type, sequence, meta} = action;
-        const actions = action.payload;
-        const [ PENDING, FULFILLED, REJECTED ] = (meta || {}).promiseTypeSuffixes || promiseTypeSuffixes;
-        let promise;
+            const { type, sequence, meta} = action;
+            const actions = action.payload;
+            const [ PENDING, FULFILLED, REJECTED ] = (meta || {}).promiseTypeSuffixes || promiseTypeSuffixes;
+            let promise;
 
-        next({
-            type: `${type}_${PENDING}`,
-            ...meta ? { meta } : {}
-        });
-
-        if (sequence) {
-            promise = actions.reduce((result, item) => result.then(() => next(item())), Promise.resolve());
-        } else {
-            promise = Promise.all(actions.map(item => next(item())));
-        }
-
-        return promise.then(
-            payload => next({
-                payload,
-                type: `${type}_${FULFILLED}`,
+            next({
+                type: `${type}_${PENDING}`,
                 ...meta ? { meta } : {}
-            }),
-            error => next({
-                payload: error,
-                error: true,
-                type: `${type}_${REJECTED}`,
-                ...meta ? { meta } : {}
-            })
-        );
+            });
+
+            if (sequence) {
+                promise = actions.reduce((result, item) => result.then(() => next(item())), Promise.resolve());
+            } else {
+                promise = Promise.all(actions.map(item => next(item())));
+            }
+
+            return promise.then(
+                payload => next({
+                    payload,
+                    type: `${type}_${FULFILLED}`,
+                    ...meta ? { meta } : {}
+                }),
+                error => next({
+                    payload: error,
+                    error: true,
+                    type: `${type}_${REJECTED}`,
+                    ...meta ? { meta } : {}
+                })
+            );
+        };
     };
 }
